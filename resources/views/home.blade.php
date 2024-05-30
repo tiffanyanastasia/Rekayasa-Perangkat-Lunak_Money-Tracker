4fafa93c060e1ec5564f2c84856d0ea892152c29
NabielMuaafiiR <142709126+NabielMuaafiiR@users.noreply.github.com>
2024-05-31 04:52:37 +0700

    Finall

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -108,6 +108,5 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/chart.js/dist/chart.umd.min.js"></script>
-    <script src="page.js"></script>
 </body>
 </html>
